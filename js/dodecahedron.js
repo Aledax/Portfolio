48f48b3b36c759dfb033e8b1f883a49f43851bb3
Aledax <dalexay16@gmail.com>
2025-06-01 23:16:12 +0900

    Added background + custom dodecahedron

---
+++ js/dodecahedron.js
@@ -0,0 +1,90 @@
+import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
+
+const radius = 1;
+
+const phi = (1 + Math.sqrt(5)) / 2;
+const a = 1.0 / Math.sqrt(3); // Cube vertex
+const b = phi / Math.sqrt(3); // Roof vertex (long)
+const c = 1 / phi / Math.sqrt(3); // Roof vertex (short)
+
+const vertices = [
+    [a, a, a],
+    [-a, a, a],
+    [a, a, -a],
+    [-a, a, -a],
+    [a, -a, a],
+    [-a, -a, a],
+    [a, -a, -a],
+    [-a, -a, -a],
+    [c, b, 0],
+    [-c, b, 0],
+    [c, -b, 0],
+    [-c, -b, 0],
+    [b, 0, c],
+    [b, 0, -c],
+    [-b, 0, c],
+    [-b, 0, -c],
+    [0, c, b],
+    [0, -c, b],
+    [0, c, -b],
+    [0, -c, -b]
+];
+
+for (let v = 0; v < 20; v++) {
+    for (let c = 0; c < 3; c++) {
+        vertices[v][c] *= radius;
+    }
+}
+
+const pentagons = [
+    [8, 9, 3, 18, 2],
+    [9, 8, 0, 16, 1],
+    [10, 11, 5, 17, 4],
+    [11, 10, 6, 19, 7],
+    [12, 13, 6, 10, 4],
+    [13, 12, 0, 8, 2],
+    [14, 15, 3, 9, 1],
+    [15, 14, 5, 11, 7],
+    [16, 17, 5, 14, 1],
+    [17, 16, 0, 12, 4],
+    [18, 19, 6, 13, 2],
+    [19, 18, 3, 15, 7]
+];
+
+const faceCenters = [];
+
+for (let p = 0; p < 12; p++) {
+    let pentagon = pentagons[p];
+    let sum = [0, 0, 0];
+    for (let v = 0; v < 5; v++) {
+        sum[0] += vertices[pentagon[v]][0];
+        sum[1] += vertices[pentagon[v]][1];
+        sum[2] += vertices[pentagon[v]][2];
+    }
+    for (let c = 0; c < 3; c++) {
+        sum[c] /= 5;
+    }
+    faceCenters.push(sum);
+}
+
+var positions = [];
+
+for (let p = 0; p < 12; p++) {
+    let pentagon = pentagons[p];
+    for (let e = 0; e < 5; e++) {
+        positions = positions.concat(vertices[pentagon[e]]);
+        positions = positions.concat(faceCenters[p]);
+        positions = positions.concat(vertices[pentagon[(e+1)%5]]);
+    }
+}
+
+console.log(positions);
+
+export function createDodecahedronGeometry() {
+
+    let geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
+    geometry.computeVertexNormals();
+
+    return geometry;
+}
--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
+import { createDodecahedronGeometry } from './dodecahedron.js';
 
 var scene, camera, renderer, clock;
-var shapeZPivot, shape;
+var shapeZPivot, shapeBack, shapeFront;
 
 const rotationMultiplier = 0.005;
+const rotationAcceleration = 0.5;
+const rotationFixedDeceleration = 0.75;
+const rotationFreeDeceleration = 0.95;
 const shapePosition = new THREE.Vector3(0, 0, 0);
 const shapeRadius = 1;
 const shapeFloatAmplitude = 0.0025;
@@ -11,8 +15,78 @@ const shapeFloatPeriod = 5;
 const shapeFloatRotationAmplitude = 0.05;
 const shapeFloatRotationOffset = shapeFloatPeriod / 2;
 
+const shapeVertexShader = `
+    attribute vec3 barycentric;
+    varying vec3 vBarycentric; // Gets passed to fragment shader
+    varying vec3 vNormal;
+
+    void main() {
+        vBarycentric = barycentric;
+        vNormal = normalize(normalMatrix * normal);
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0); // These three values are provided by Three.js
+    }
+`;
+
+const shapeBackFragmentShader = `
+    precision mediump float; // Specify float precision
+    varying vec3 vBarycentric; // Comes from vertex shader
+    varying vec3 vNormal;
+
+    void main() {
+        float lighting = -vNormal.y * 0.25 + 0.75;
+        float edgeCloseness = vBarycentric.y;
+        float alpha = 0.0;
+        float opaqueAlpha = 0.4;
+        float transparentAlpha = 0.0;
+        float opaqueBarrier = 0.01;
+        float transparentBarrier = 0.02;
+        if (edgeCloseness < opaqueBarrier) {
+            alpha = opaqueAlpha;
+        } else if (edgeCloseness < transparentBarrier) {
+            alpha = opaqueAlpha + (transparentAlpha - opaqueAlpha) * (edgeCloseness - opaqueBarrier) / (transparentBarrier - opaqueBarrier);
+        } else {
+            alpha = transparentAlpha;
+        }
+        gl_FragColor = vec4(vec3(0.0, 0.7, 1.0) * lighting, alpha);
+    }
+`
+
+const shapeFrontFragmentShader = `
+    precision mediump float; // Specify float precision
+    varying vec3 vBarycentric; // Comes from vertex shader
+    varying vec3 vNormal;
+
+    void main() {
+        float maxLightFactor = 0.75;
+        float minDarkFactor = 0.25;
+        float lightFactor = 0.0;
+        float darkFactor = 1.0;
+        if (vNormal.y >= 0.0) {
+            lightFactor = vNormal.y * maxLightFactor;
+        } else {
+            darkFactor = 1.0 + vNormal.y * (1.0 - minDarkFactor);
+        }
+
+        float edgeCloseness = vBarycentric.y;
+        float alpha = 0.0;
+        float opaqueAlpha = 1.0;
+        float transparentAlpha = 0.2;
+        float opaqueBarrier = 0.04;
+        float transparentBarrier = 0.06;
+        if (edgeCloseness < opaqueBarrier) {
+            alpha = opaqueAlpha;
+        } else if (edgeCloseness < transparentBarrier) {
+            alpha = opaqueAlpha + (transparentAlpha - opaqueAlpha) * (edgeCloseness - opaqueBarrier) / (transparentBarrier - opaqueBarrier);
+        } else {
+            alpha = transparentAlpha;
+        }
+        gl_FragColor = vec4((vec3(0.0, 0.7, 1.0) + lightFactor) * darkFactor, alpha);
+    }
+`
+
 var mousedown = false;
 var mousepos = new THREE.Vector2();
+var mousevelocity = new THREE.Vector2();
 var scenemousepos = new THREE.Vector2();
 var raycaster = new THREE.Raycaster();
 
@@ -22,12 +96,16 @@ function init() {
 
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(
-        40,
+        50,
         window.innerWidth/window.innerHeight,
         0.1,
         1000);
-    renderer = new THREE.WebGLRenderer();
+    renderer = new THREE.WebGLRenderer({
+        alpha: true,
+        antialias: true
+    });
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(0x000000, 0);
     clock = new THREE.Clock();
     clock.start();
 
@@ -36,10 +114,15 @@ function init() {
     document.addEventListener('mouseup', e => onRelease(e.clientX, e.clientY), false);
     document.addEventListener('mousemove', e => onMove(e.clientX, e.clientY), false);
     document.addEventListener('touchstart', e => onPress(e.touches[0].clientX, e.touches[0].clientY), false);
-    document.addEventListener('touchend', e => onRelease(e.touches[0].clientX, e.touches[0].clientY), false);
+    document.addEventListener('touchend', e => {
+        if (mousedown) {
+            e.preventDefault();
+        }
+        onRelease(e.touches[0].clientX, e.touches[0].clientY);
+    }, { passive: false });
     document.addEventListener('touchmove', e => {
         if (mousedown) {
-            e.preventDefault(); // Prevent scroll
+            e.preventDefault();
         }
         onMove(e.touches[0].clientX, e.touches[0].clientY);
     }, { passive: false });
@@ -49,14 +132,41 @@ function init() {
     shapeZPivot = new THREE.Object3D();
     scene.add(shapeZPivot);
 
-    const geometry = new THREE.IcosahedronGeometry(shapeRadius);
-    const material = new THREE.MeshNormalMaterial();
-    shape = new THREE.Mesh(geometry, material);
-    shapeZPivot.add(shape);
+    const geometry = createDodecahedronGeometry(shapeRadius);
+    const barycentric = [];
+    for (let i = 0; i < 60; i++) {
+        barycentric.push(1, 0, 0);
+        barycentric.push(0, 1, 0);
+        barycentric.push(0, 0, 1);
+    }
+    geometry.setAttribute('barycentric', new THREE.Float32BufferAttribute(barycentric, 3));
+    
+    const shapeBackMaterial = new THREE.ShaderMaterial({
+        transparent: true,
+        side: THREE.BackSide,
+        vertexShader: shapeVertexShader,
+        fragmentShader: shapeBackFragmentShader
+    });
+    const shapeFrontMaterial = new THREE.ShaderMaterial({
+        transparent: true,
+        vertexShader: shapeVertexShader,
+        fragmentShader: shapeFrontFragmentShader
+    });
+    shapeBack = new THREE.Mesh(geometry, shapeBackMaterial);
+    shapeFront = new THREE.Mesh(geometry, shapeFrontMaterial);
+    shapeZPivot.add(shapeBack);
+    shapeZPivot.add(shapeFront);
 
     camera.position.z = 5;
 
     animate();
+}
+
+function rotateShape() {
+    const q = new THREE.Quaternion();
+    q.setFromAxisAngle(new THREE.Vector3(mousevelocity.y, mousevelocity.x, 0).normalize(), rotationMultiplier * mousevelocity.length());
+    shapeBack.quaternion.premultiply(q);
+    shapeFront.quaternion.premultiply(q);
 }
 
 function animate() {
@@ -68,6 +178,13 @@ function animate() {
 
     const floatY = shapeFloatAmplitude * Math.cos(clock.getElapsedTime() * Math.PI * 2 / shapeFloatPeriod);
     shapeZPivot.position.copy(shapePosition.add(new THREE.Vector3(0, floatY, 0)));
+
+    if (mousedown) {
+        mousevelocity.multiplyScalar(rotationFixedDeceleration);
+    } else {
+        mousevelocity.multiplyScalar(rotationFreeDeceleration);
+    }
+    rotateShape();
 
     renderer.render(scene, camera);
 }
@@ -100,17 +217,14 @@ function onRelease(x, y) {
 function onMove(x, y) {
 
     if (mousedown) {
-        const dx = (x - mousepos.x);
-        const dy = (y - mousepos.y);
-        const q = new THREE.Quaternion();
-        q.setFromAxisAngle(new THREE.Vector3(dy, dx, 0).normalize(), rotationMultiplier * new THREE.Vector2(dx, dy).length());
-        shape.quaternion.premultiply(q);
+        mousevelocity.add(new THREE.Vector2(x - mousepos.x, y - mousepos.y).multiplyScalar(rotationAcceleration));
     }
 
     mousepos = new THREE.Vector2(x, y);
 }
 
 function onWindowResize() {
+
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 
